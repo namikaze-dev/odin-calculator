9e6c397017539ba11005697ce11ab69899db0d3d
Abubakar Usman <usmansadik96@gmail.com>
2025-10-20 01:07:15 +0100

    feat: add content and style for calc ui

---
+++ script.js
@@ -0,0 +1,33 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+let numberA = 0;
+let numberB = 0;
+let operator;
+
+const operate = (operator, numberA, numberB) => {
+  let result;
+  switch (operator) {
+    case "+":
+      result = add(numberA, numberB);
+      break;
+    case "-":
+      result = subtract(numberA, numberB);
+      break;
+    case "*":
+      result = multiply(numberA, numberB);
+      break;
+    case "/":
+      result = divide(numberA, numberB);
+      break;
+    default:
+      console.log("Invalid operator:", operator)
+  }
+  return result;
+}
+
+const display = document.querySelector(".display");
+const displayTxt = display.querySelector(".display-txt");
+displayTxt.textContent = "122"
